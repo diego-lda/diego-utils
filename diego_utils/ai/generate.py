dc5f9799226cf07d487c06b43b240f3bea7a2938
Diego Lara <diegolaradeandres@gmail.com>
2025-01-25 13:17:33 +0300

    Feat: Widen model options

--- diego_utils/ai/generate.py
+++ diego_utils/ai/generate.py
@@ -12,7 +12,7 @@ from langchain_huggingface import HuggingFaceEndpoint
 def generate_response(
     question: str,
     hf_key: str,
-    model_id: str = "mistralai/Mistral-7B-Instruct-v0.3",
+    model_id: str = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
 ) -> str:
     """Generates a response from the LLM model.
 
@@ -49,6 +49,35 @@ if __name__ == "__main__":
     # Load the token for Hugging Face
     load_dotenv()
     sec_key = os.getenv("HF_TOKEN")
-    sample_question = "What is the capital of France?"
-    response = generate_response(question=sample_question, hf_key=sec_key)
-    print(response)
+    # sample_question = "What is the capital of France?"
+    # response = generate_response(question=sample_question, hf_key=sec_key)
+    # print(response)
+
+    from huggingface_hub import InferenceClient
+
+    client = InferenceClient(api_key=sec_key)
+
+    messages = [
+        {"role": "user", "content": "Can you generate a training plan for cycling?"},
+    ]
+
+    completion = client.chat.completions.create(
+        model="meta-llama/Meta-Llama-3-8B-Instruct",
+        messages=messages,
+        max_tokens=500,
+    )
+
+    print(completion.choices[0].message)
+
+    # Use a pipeline as a high-level helper
+    from transformers import pipeline
+
+    messages = [
+        {"role": "user", "content": "Who are you?"},
+    ]
+    pipe = pipeline(
+        "text-generation",
+        model="deepseek-ai/DeepSeek-R1",
+        trust_remote_code=True,
+    )
+    pipe(messages)
